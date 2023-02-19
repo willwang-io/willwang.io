40790d3941d02e05ec620dad09845abd616d8d11
ge-wu <jiayewang65@gmail.com>
2023-02-19 11:57:23 -0600

    [Bug] Save current theme to local storage
    When change to another page, the theme will set back to the original
    theme, the light here. To solve this, we save the theme variable to
    local storage.

--- components/ColorModeContext.js
+++ components/ColorModeContext.js
@@ -1,7 +1,16 @@
-import { createContext, useContext, useMemo, useState } from "react";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import { darkTheme, lightTheme } from "../assets/theme";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { useEventCallback } from "@mui/material";
 
 const ColorModeContext = createContext({ toggleColorMode: () => {} });
 
@@ -9,21 +18,32 @@ export function useModeContext() {
   return useContext(ColorModeContext);
 }
 
+function useLocalStorage(key, fallbackValue) {
+  const [value, setValue] = useState(fallbackValue);
+  useEffect(() => {
+    const stored = localStorage.getItem(key);
+    setValue(stored ? JSON.parse(stored) : fallbackValue);
+  }, [fallbackValue, key]);
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+}
+
 export default function ThemeContext({ children }) {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-  const colorMode = useMemo(
-    () => ({
-      toggleColorMode: () => {
-        setIsDarkMode((prevMode) => !prevMode);
-      },
-    }),
-    []
-  );
-  const theme = isDarkMode ? darkTheme : lightTheme;
+  const [isDarkMode, setDarkMode] = useLocalStorage("theme", false);
+
+  const colorMode = {
+    toggleColorMode: () => {
+      setDarkMode((prevMode) => !prevMode);
+    },
+  };
 
   return (
     <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={isDarkMode ? darkTheme : lightTheme}>
         <CssBaseline />
         {children}
       </ThemeProvider>
