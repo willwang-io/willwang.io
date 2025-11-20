86e3aefeb1b43c2ad488bfccf1b4a6af97e2fcd3
willwang-io <brill.13.koi@icloud.com>
2025-11-20 11:40:29 -0600

    Remove unwanted files

--- src/lib.rs
+++
@@ -1,4 +0,0 @@
-pub mod ast;
-pub mod file_util;
-pub mod parser;
-pub mod render;
--- templates/js/mathjax-config.js
+++
@@ -1,7 +0,0 @@
-window.MathJax = {
-  tex: { inlineMath: [['$', '$'], ['\\(', '\\)']], displayMath: [['$$','$$'], ['\\[','\\]']] },
-  loader: {
-    paths: { font: 'https://cdn.jsdelivr.net/npm/@mathjax' },
-    load: ['[font]/mathjax-euler-font']
-  }
-};
